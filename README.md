6bcc82a39de11a9bc3a58d855a02d49e37c1e471
Denis Ryzhkov <denisr@denisr.com>
2013-02-01 11:37:07 +0300

    Improved docs layout.

--- README.md
+++ README.md
@@ -3,12 +3,6 @@ uniqtoken
 
 Simple generator of unique tokens.
 
-* Starts with a sequence random enough to be indexed efficiently when token is used as a hash key.
-* This random sequence is long enough to be hard to guess.
-* Ends with explicit timestamp with precision about 0.01 seconds (system dependent) to be more unique.
-* Is more than 3x faster than standard `uuid.uuid4()` - see tests in [`uniqtoken.py:test()`](https://github.com/denis-ryzhkov/uniqtoken/blob/master/uniqtoken.py#L41).
-* Is ready to be used as a fixed length string token containing exactly 64 safe hex digits.
-
 Usage:
 
     pip install uniqtoken
@@ -18,6 +12,12 @@ Usage:
     token2 = uniqtoken()
     assert token1 != token2
 
-uniqtoken.py version 0.1.1  
+* Starts with a sequence random enough to be indexed efficiently when token is used as a hash key.
+* This random sequence is long enough to be hard to guess.
+* Ends with explicit timestamp with precision about 0.01 seconds (system dependent) to be more unique.
+* Is more than 3x faster than standard `uuid.uuid4()` - see tests in [`uniqtoken.py:test()`](https://github.com/denis-ryzhkov/uniqtoken/blob/master/uniqtoken.py#L41).
+* Is ready to be used as a fixed length string token containing exactly 64 safe hex digits.
+
+uniqtoken.py version 0.1.2  
 Copyright (C) 2013 by Denis Ryzhkov <denisr@denisr.com>  
 MIT License, see http://opensource.org/licenses/MIT
--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@ from distutils.core import setup
 
 setup(
     name='uniqtoken',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple generator of unique tokens.',
     long_description='''
-* Starts with a sequence random enough to be indexed efficiently when token is used as a hash key.
-* This random sequence is long enough to be hard to guess.
-* Ends with explicit timestamp with precision about 0.01 seconds (system dependent) to be more unique.
-* Is more than 3x faster than standard ``uuid.uuid4()`` - see tests in `uniqtoken.py:test() <https://github.com/denis-ryzhkov/uniqtoken/blob/master/uniqtoken.py#L41>`_.
-* Is ready to be used as a fixed length string token containing exactly 64 safe hex digits.
-
 Usage::
 
     pip install uniqtoken
@@ -19,6 +13,13 @@ Usage::
     token1 = uniqtoken()
     token2 = uniqtoken()
     assert token1 != token2
+
+* Starts with a sequence random enough to be indexed efficiently when token is used as a hash key.
+* This random sequence is long enough to be hard to guess.
+* Ends with explicit timestamp with precision about 0.01 seconds (system dependent) to be more unique.
+* Is more than 3x faster than standard ``uuid.uuid4()`` - see tests in `uniqtoken.py:test() <https://github.com/denis-ryzhkov/uniqtoken/blob/master/uniqtoken.py#L41>`_.
+* Is ready to be used as a fixed length string token containing exactly 64 safe hex digits.
+
 ''',
     url='https://github.com/denis-ryzhkov/uniqtoken',
     author='Denis Ryzhkov',
